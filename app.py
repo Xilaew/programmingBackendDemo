d707ab2dc397eeabab344bba2d820830d823b6f6
Felix von Poblotzki <xilaew@gmail.com>
2021-04-11 01:24:20 +0200

    python runtime not supported by heroku

--- app.py
+++ app.py
@@ -2,24 +2,61 @@ from flask import Flask, Response
 from flask import request
 import os
 import redis
+import fakeredis
+import json
 
 app = Flask(__name__)
 
-r = redis.from_url(os.environ.get("REDIS_URL"))
+#r = redis.from_url(os.environ.get("REDIS_URL"))
+r = fakeredis.FakeStrictRedis()
 
-@app.route('/')
+def store(key, value):
+    r.set(key,value)
+
+def get(key):
+    result = r.get(key)
+    if type(result) == bytes:
+        return result.decode("utf-8")
+    else:
+        return result
+
+@app.route('/', methods = ['GET'])
 def homepage():
-    play_id = request.args.get('app_id')
-    play_id = 'tech.jonas.travelbudget' if play_id is None else play_id
-
     return """
-    <h1>Whats this app about</h1>
-    <form action='/' method='get'>
-      <input type='text' name='app_id'></input>
+    <h1>An input form</h1>
+    <form action='/submit' method='post'>
+      <label>key</label><input type='text' name='key'></input>
+      <label>value</label><input type='text' name='value'></input>
       <input type='submit'></input>
     </form>
-    <img src="/png/{play_id}">
-    """.format(play_id=play_id)
+    """
+
+@app.route('/submit', methods = ['POST'])
+def submit():
+    key = request.form['key']
+    value = request.form['value']
+    previous = get(key)
+    store(key,value)
+    result = {"previous": previous, "key": key, "value": value}
+    return json.dumps(result)
+
+@app.route('/judge', methods = ['GET'])
+def judge():
+    result = ""
+    cursor = '0'
+    while cursor != 0:
+        cursor, keys = r.scan(cursor=cursor)
+        values = r.mget(*keys)
+        for (key, value) in (zip(keys, values)):
+            result += "<tr><td>{key}</td><td>{value}</td></tr>".format(key=key.decode("utf-8"), value=value.decode("utf-8"))
+    result = "<table>{result}</table>".format(result=result)
+    return result
+
+    value = request.form['value']
+    previous = get(key)
+    store(key,value)
+    result = {"previous": previous, "key": key, "value": value}
+    return json.dumps(result)
 
 
 if __name__ == '__main__':
