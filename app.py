8c4280ce1aa8c354dc736988846fa3095bd96a28
Felix von Poblotzki <xilaew@gmail.com>
2021-11-21 21:47:15 +0100

    added Clear function

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, Response
 from flask import request, redirect, url_for
 import os
 import redis
-#import fakeredis
+import fakeredis
 import json
 
 app = Flask(__name__)
 
-r = redis.from_url(os.environ.get("REDIS_URL"))
-#r = fakeredis.FakeStrictRedis()
+#r = redis.from_url(os.environ.get("REDIS_URL"))
+r = fakeredis.FakeStrictRedis()
 
 
 def store(key, value):
-    r.set(key,value)
+    r.set(key, value)
 
 
 def get(key):
@@ -23,7 +23,7 @@ def get(key):
         return result
 
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def homepage():
     return """
     <html>
@@ -43,39 +43,61 @@ def homepage():
     """
 
 
-@app.route('/submit', methods = ['POST'])
+@app.route('/submit', methods=['POST'])
 def submit():
     key = request.form['key']
     value = request.form['value']
     previous = get(key)
-    store(key,value)
+    store(key, value)
     result = {"previous": previous, "key": key, "value": value}
     return json.dumps(result)
 
 
-@app.route('/judge', methods = ['GET'])
+def decode(redisvalue):
+    if type(redisvalue) == bytes:
+        return redisvalue.decode("utf-8")
+    if redisvalue == None:
+        return ""
+    else:
+        return redisvalue
+
+
+def header(l):
+    return "<th></th>" + "".join(["<th>{}</th>".format(i) for i in l])
+
+
+def row(l, v):
+    keys = ["{}{}".format(v, i) for i in l]
+    values = r.mget(*keys)
+    return "<tr><th>{v}</th>{content}</tr>".format(v=v, content="".join(["<td>{}</td>".format(decode(value)) for value in values]))
+
+
+@app.route('/judge', methods=['GET'])
 def judge():
+    l = [str(chr(x)) for x in range(ord('A'), ord('Z') + 1)]
+    table1 = "<table><tr>{head}</tr>{body}</table>".format(head=header(l), body="".join([row(l, v) for v in l]))
     result = ""
     cursor = '0'
     while cursor != 0:
         cursor, keys = r.scan(cursor=cursor)
+        if len(keys) <= 0:
+            break
         values = r.mget(*keys)
         for (key, value) in (zip(keys, values)):
             result += "<tr><td>{key}</td><td>{value}</td></tr>".format(key=key.decode("utf-8"), value=value.decode("utf-8"))
-    result = "<table>{result}</table>".format(result=result)
+    result = "{table1}<hr><table>{result}</table>".format(table1=table1, result=result)
     return result
 
 
-app.route('/clear', methods = ['POST'])
+@app.route('/clear', methods=['POST'])
 def post_clear():
     r.flushdb()
-    return redirect(url_for(judge))
+    return redirect(url_for('judge'))
 
 
-app.route('/clear', methods = ['GET'])
+@app.route('/clear', methods=['GET'])
 def get_clear():
-    return app.send_static_file()
-
+    return app.send_static_file("clear.html")
 
 
 if __name__ == '__main__':
