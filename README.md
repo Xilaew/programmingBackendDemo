afed04ed91d83dd901f6fdf6945e012f772509e6
Felix von Poblotzki <felix.vonpoblotzki@yourstruly.de>
2023-02-27 07:40:03 +0100

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a great sinking ships app
+
+# quickstart
+'''flask --app app.py --debug run''' 
--- win.py
+++ win.py
@@ -1,5 +1,4 @@
 import requests
-headers = {'User-Agent': 'Mozilla/5.0'}
-payload = {'key':'test', 'value':'123456'}
-r = requests.post('https://codingschooldemo.herokuapp.com/submit', headers=headers, data=payload)
-print(r)
+payload = {'key':'BB', 'value':'Gewinner'}
+r = requests.post('https://codingschooldemo.herokuapp.com/submit', data=payload)
+print(r.text)
